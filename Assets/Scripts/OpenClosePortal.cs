924f610c81f3039c1c10bed7ec05f53279bc9b7f
Jayne Gale <jaynegale@gmail.com>
2019-01-30 23:53:51 +1300

    Portal animations open and close on button press

---
+++ Assets/Scripts/OpenClosePortal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenClosePortal : MonoBehaviour {
+
+	public GameObject portalButton;
+	public bool isOpening;
+	Animator animator;
+
+	void Start () 
+	{
+		ButtonClicker buttonClicker; 
+		buttonClicker = portalButton.GetComponent<ButtonClicker>();
+		buttonClicker.OnClicked += Button_OnClicked;
+		animator = gameObject.GetComponent<Animator>();
+		isOpening = false;
+	}
+
+	void Button_OnClicked (bool buttonState)
+	{
+		animator.SetBool("isOpening", buttonState);
+	}
+}
