cc4811b96c1cc51f4b97402939998a186884f834
Jayne Gale <jaynegale@gmail.com>
2019-02-05 23:08:54 +1300

    Trigger portal to close on exit or entry

---
+++ Assets/ClosePortalOnExit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosePortalOnExit : MonoBehaviour {
+    public GameObject portalButton1;
+    public GameObject portalButton2;
+            
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            portalButton1.GetComponent<ButtonClicker>().DeactivateButton();
+            portalButton2.GetComponent<ButtonClicker>().DeactivateButton();
+        }
+    }
+
+}
