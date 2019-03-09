e8e7b9c829e465b5c6247a07b4c37d8a7e4926a8
Jayne Gale <jaynegale@gmail.com>
2019-03-10 00:42:50 +1300

    Button animations not working

---
+++ Assets/Scripts/PortalClosingVolume.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PortalClosingVolume : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject fountain;
+    public GameObject portalDoor;
+    float fountainMaxVol;
+    float portalMaxVol;
+    AudioSource fountainAudio;
+    AudioSource portalAudio;
+    AudioSource onAudio;
+    AudioSource rampAudio;
+    Animator portalAnimator;
+    float portalDoorPos;
+    float playerPos;
+    bool insideRoom;
+    bool isOpening;
+    float volumeSetting;
+    int upOrDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        portalAnimator = gameObject.GetComponent<Animator>();
+        portalAudio = gameObject.GetComponent<AudioSource>();
+        portalMaxVol = portalAudio.volume;
+        fountainAudio = fountain.GetComponent<AudioSource>();
+        fountainMaxVol = fountainAudio.volume;
+        playerPos = player.transform.position.x;
+        insideRoom = (playerPos < portalDoorPos) ? true : false;
+        onAudio = (insideRoom) ? fountainAudio : portalAudio;
+        onAudio.volume = (insideRoom)? fountainMaxVol: portalMaxVol;
+        onAudio.Play();
+        rampAudio.volume = 0.0f;
+        rampAudio.Play();
+    }
+
+    void PortalAnimating()
+    {
+        isOpening = portalAnimator.GetBool("isOpening");
+        upOrDown = (isOpening) ? 1 : -1;
+        portalDoorPos = portalDoor.transform.position.x;
+        playerPos = player.transform.position.x;
+        insideRoom = (playerPos < portalDoorPos) ? true : false;
+        onAudio = (insideRoom) ? fountainAudio : portalAudio;
+        onAudio.volume = (insideRoom) ? fountainMaxVol : portalMaxVol;
+        onAudio.Play();
+        rampAudio = (insideRoom) ? portalAudio : fountainAudio;
+        if(rampAudio.volume >= 0.0f && rampAudio.volume <= 1.0f)
+        {
+            rampAudio.volume += upOrDown * Time.deltaTime;
+        }
+    }
+}
