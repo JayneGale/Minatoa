c07ce86799cb0942f2e535ff1297cc0aafc55ba1
Jayne Gale <jaynegale@gmail.com>
2019-04-01 23:26:17 +1300

    A placeholder main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    private Scene scene;
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ReallyQuit()
+    {
+        Debug.Log("Quit confirmation has been pressed");
+        Application.Quit();
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
